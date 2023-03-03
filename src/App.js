601e54578f19d04895d5b0de007671de3061e799
soykan <soykanerturk@gmail.com>
2023-03-03 21:23:06 +0300

    listing thingsToDo completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+let thingsToDo = ["Play a game!", "Watch a movie!", 
+                  "Read a book!", "Take a walk!"];
 
 function App() {
   return (
     <div>
-      <h1>What to Do App</h1>
+        <h1>What to Do App</h1>
+
+        <ol>
+          {thingsToDo.map((item) => {
+
+              return <li>{item}</li>;
+
+          })}
+        </ol>
+        
     </div>
   );
 }
